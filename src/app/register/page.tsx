a70150504c5faf89f1f9c54cfe91281f47521ce5
alongkornpp <alongkorn.p@cloudnctech.com>
2024-11-13 13:13:15 +0700

    create register page and create function manage about register logic

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,85 @@
+"use client";
+
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export default function Page() {
-    return (
-        <div>
-            Register Page
-        </div>
-    )
+  const [data, setData] = useState({});
+
+  const handleChange = (e: any) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    const response = await axios.post(
+      `http://localhost:8000/api/auth/register`,
+      data
+    );
+
+    if (response.data.status !== "success") {
+      await alert("Somthing went wrong");
+    }
+
+    await alert(response.data.message);
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="">First Name</label>
+        <input
+          type="text"
+          name="firstname"
+          placeholder="First Name"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div>
+        <label htmlFor="">Last Name</label>
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Last Name"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div>
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div>
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div>
+        <label htmlFor="">Class</label>
+        <input
+          type="text"
+          name="class"
+          placeholder="Class"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div>
+        <label htmlFor="">Number</label>
+        <input
+          type="text"
+          name="number"
+          placeholder="Number"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <button onClick={handleSubmit}>Register</button>
+    </>
+  );
 }
