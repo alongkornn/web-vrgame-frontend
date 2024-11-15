2c1336e41caacda149533bd559d7d88379130788
alongkornpp <alongkorn.p@cloudnctech.com>
2024-11-15 12:07:22 +0700

    create folder login mange about login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,54 @@
-export default function Login() {
-    return (
-        <>
-            <h1>Login Page</h1>
-        </>
-    )
-}
+"use client";
 
+import axios from "axios";
+import { useState } from "react";
+
+export default function Login() {
+  const [data, setData] = useState([{}]);
+
+  const handleChage = (e: any) => {
+    setData(() => [
+      {
+        ...data,
+        [e.target.name]: e.target.value
+      }
+    ]);
+  };
+
+  const handleSubmit = async () => {
+    const response = await axios.post(
+      "http://localhost:8000/api/auth/login",
+      data
+    );
+
+    if (response.data.message !== "success") {
+      await alert("Fail to login");
+    }
+
+    await alert(response.data.message);
+  };
+
+  console.log(data);
+
+  return (
+    <>
+      <form>
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={(e) => handleChage(e)}
+        />
+        <br />
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => handleChage(e)}
+        />
+      </form>
+    </>
+  );
+}
