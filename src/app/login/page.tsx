c3a62330e8bde6dd5bcbce663798a142e0330133
alongkornpp <alongkorn.p@cloudnctech.com>
2024-11-27 15:35:16 +0700

    crate form input for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
 
-function LoginPage() {
+import axios from "axios";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [data, setData] = useState({});
+
+  const handleChange = (e: any) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    const response = await axios.post(
+      "http://localhost:8000/api/auth/login",
+      data
+    );
+
+    if (response.data.status !== "success") {
+      await alert("Fail to login");
+    }
+
+    await alert(response.data.message);
+  };
+
   return (
-    <div>LoginPage</div>
-  )
+    <>
+      <form>
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => handleChange(e)}
+        />
+      </form>
+      <button onClick={handleSubmit}>Login</button>
+    </>
+  );
 }
-
-export default LoginPage
