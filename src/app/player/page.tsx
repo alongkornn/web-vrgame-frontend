adcec80bf1bac70ad5af957b62e9024ce41ba18e
alongkornpp <alongkorn.p@cloudnctech.com>
2024-11-27 17:01:36 +0700

    use API_URL in path get all user

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+
 import { User } from "../../../utils/user/user";
+import { API_URL } from "../../../utils/api-url/api.url";
 
 function Player() {
   const [data, setData] = useState<User[]>([]);
 
   const fetch = async () => {
     try {
-      const response = await axios.get("http://localhost:8000/api/user");
+      const response = await axios.get(`${API_URL}/user`);
       setData(response.data.data);
     } catch (error) {
       console.error("Error fetching data:", error);
